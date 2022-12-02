ebea1ad837e4a94dd2fcdf2824a78c5fa96e96e1
Yijin Ro <yri.carmine@gmail.com>
2022-12-01 20:11:46 -0500

    review association set up

--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,8 @@ const User = require("./User");
 const seed = require("./seed");
 const Book = require("./Book");
 const Cart = require("./Cart");
+const Review = require("./Review");
+
 // ASSOCIATIONS HERE!
 Cart.hasMany(User);
 User.belongsTo(Cart);
@@ -10,9 +12,16 @@ User.belongsTo(Cart);
 Cart.belongsToMany(Book, { through: "CartBooks" });
 Book.belongsToMany(Cart, { through: "CartBooks" });
 
+Book.hasMany(Review);
+Review.belongsTo(Book);
+
+User.hasMany(Review);
+Review.belongsTo(User);
+
 module.exports = {
   seed,
   User,
   db,
   Book,
+  Review
 };
