02f9e0fd8ebf3b95b6cfa2383957f79fbae8b300
Rosalie Newman <rosalienewman0@gmail.com>
2022-12-01 21:08:20 -0500

    changed user phonenumber to string, changed creditcard to bigint, fixed small bug in review.js and seed.js builds

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,64 +1,64 @@
-const db = require("./db");
-const User = require("./User");
+const db = require('./db');
+const User = require('./User');
 
 const seed = async () => {
-  await db.sync({ force: true });
+	await db.sync({ force: true });
 
-  const [moe, lucy, larry, ethyl] = await Promise.all([
-    User.create({
-      username: "moe",
-      password: "123",
-      firstName: "Moe",
-      lastName: "Jones",
-      email: "moejones99@email.com",
-      birthday: "1990-03-20",
-      phoneNumber: 9785477082,
-      address: "1000 Pine Street",
-      creditCard: 1234123412341234,
-    }),
-    User.create({
-      username: "lucy",
-      password: "123",
-      firstName: "Lucy",
-      lastName: "Smith",
-      email: "lucysmith100@email.com",
-      birthday: "1990-12-25",
-      phoneNumber: 978547800,
-      address: "95 Grace Way",
-      creditCard: 1235123512351235,
-    }),
-    User.create({
-      username: "larry",
-      password: "123",
-      firstName: "Larry",
-      lastName: "Johnson",
-      email: "larryjohnson@email.com",
-      birthday: "2001-01-05",
-      phoneNumber: 9785446445,
-      address: "200 Hopper Ave",
-      creditCard: 2234223422342234,
-    }),
-    User.create({
-      username: "ethyl",
-      password: "123",
-      firstName: "Ethyl",
-      lastName: "Thompson",
-      email: "ethylthompson22@email.com",
-      birthday: "1995-04-12",
-      phoneNumber: 9785474422,
-      address: "22 West Way",
-      creditCard: 1254125412541254,
-    }),
-  ]);
+	const [moe, lucy, larry, ethyl] = await Promise.all([
+		User.create({
+			username: 'moe',
+			password: '123',
+			firstName: 'Moe',
+			lastName: 'Jones',
+			email: 'moejones99@email.com',
+			birthday: '1990-03-20',
+			phoneNumber: '9785477082',
+			address: '1000 Pine Street',
+			creditCard: 4846540980335660,
+		}),
+		User.create({
+			username: 'lucy',
+			password: '123',
+			firstName: 'Lucy',
+			lastName: 'Smith',
+			email: 'lucysmith100@email.com',
+			birthday: '1990-12-25',
+			phoneNumber: '9785478000',
+			address: '95 Grace Way',
+			creditCard: 346251528305693,
+		}),
+		User.create({
+			username: 'larry',
+			password: '123',
+			firstName: 'Larry',
+			lastName: 'Johnson',
+			email: 'larryjohnson@email.com',
+			birthday: '2001-01-05',
+			phoneNumber: '9785446445',
+			address: '200 Hopper Ave',
+			creditCard: 6011246417965285,
+		}),
+		User.create({
+			username: 'ethyl',
+			password: '123',
+			firstName: 'Ethyl',
+			lastName: 'Thompson',
+			email: 'ethylthompson22@email.com',
+			birthday: '1995-04-12',
+			phoneNumber: '9785474422',
+			address: '22 West Way',
+			creditCard: 5217487872137994,
+		}),
+	]);
 
-  return {
-    users: {
-      moe,
-      lucy,
-      larry,
-      ethyl,
-    },
-  };
+	return {
+		users: {
+			moe,
+			lucy,
+			larry,
+			ethyl,
+		},
+	};
 };
 
 module.exports = seed;
