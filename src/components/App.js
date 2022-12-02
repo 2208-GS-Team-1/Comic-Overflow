fc6e9e83901017b98c4ca2cf3200f89180d85309
Rosalie Newman <rosalienewman0@gmail.com>
2022-12-02 18:09:22 -0500

    changed webpack to allow imports that are .jsx to not need the file extention in the import name

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,34 @@
-import React from "react";
+import React from 'react';
 // import Home from './Home';
-import Home from "./Home/Home.jsx";
-import { Link, Routes, Route } from "react-router-dom";
-import Books from "./Books.jsx";
-import Account from "./Account.jsx";
-import Login from "./Login.jsx";
+import Home from './Home/Home.jsx';
+import { Link, Routes, Route } from 'react-router-dom';
+import Books from './Books';
+import Account from './Account.jsx';
+import Login from './Login.jsx';
 
 const App = () => {
-  return (
-    <div>
-      <div className="main_header">
-        <h1>Comic Overflow</h1>
-      </div>
+	return (
+		<div>
+			<div className="main_header">
+				<h1>Comic Overflow</h1>
+			</div>
 
-      <div>
-        <nav className="navbar">
-          <Link to="/">Home</Link>
-          <Link to="/books">Books</Link>
-          <Link to="/login">Log-In</Link>
-          <Link to="/createaccount">Create Account</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/books" element={<Books />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/createaccount" element={<Account />} />
-        </Routes>
-      </div>
-    </div>
-  );
+			<div>
+				<nav className="navbar">
+					<Link to="/">Home</Link>
+					<Link to="/books">Books</Link>
+					<Link to="/login">Log-In</Link>
+					<Link to="/createaccount">Create Account</Link>
+				</nav>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/books" element={<Books />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/createaccount" element={<Account />} />
+				</Routes>
+			</div>
+		</div>
+	);
 };
 
 export default App;
