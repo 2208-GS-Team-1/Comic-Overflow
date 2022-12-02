9cc2301679c0ef0e0027317cdec66ad1776de833
Rosalie Newman <rosalienewman0@gmail.com>
2022-12-01 22:32:19 -0500

    installed formik and began writing the form for create account. webpack hates .jsx files so paused.

--- src/components/App.js
+++ src/components/App.js
@@ -5,42 +5,44 @@ import { setUser } from '../store/userSlice';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, Routes, Route } from 'react-router-dom';
 import axios from 'axios';
+import CreateAccountForm from './CreateAccountForm';
 
 const App = () => {
-    const { user } = useSelector(state => state.user);
-    const dispatch = useDispatch();
+	const { user } = useSelector(state => state.user);
+	const dispatch = useDispatch();
 
-    const loginWithToken = async () => {
-        const token = window.localStorage.getItem('token');
-        if (token) {
-            const response = await axios.get('/api/auth', {
-                headers: {
-                    authorization: token
-                }
-            });
+	const loginWithToken = async () => {
+		const token = window.localStorage.getItem('token');
+		if (token) {
+			const response = await axios.get('/api/auth', {
+				headers: {
+					authorization: token,
+				},
+			});
 
-            dispatch(setUser(response.data))
-        }
-    };
+			dispatch(setUser(response.data));
+		}
+	};
 
-    useEffect(() => {
-        loginWithToken();
-    }, []);
+	useEffect(() => {
+		loginWithToken();
+	}, []);
 
-    if (!user.id) return <Login />
-    return (
-        <div>
-            <h1>Acme Shopping</h1>
-            <div>
-                <nav>
-                    <Link to='/'>Home</Link>
-                </nav>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                </Routes>
-            </div>
-        </div>
-    );
+	return <CreateAccountForm />;
+	// if (!user.id) return <Login />
+	// return (
+	//     <div>
+	//         <h1>Acme Shopping</h1>
+	//         <div>
+	//             <nav>
+	//                 <Link to='/'>Home</Link>
+	//             </nav>
+	//             <Routes>
+	//                 <Route path='/' element={<Home />} />
+	//             </Routes>
+	//         </div>
+	//     </div>
+	// );
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-react']
-        }
-      }
-    ]
-  }
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-react'],
+				},
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
 };
