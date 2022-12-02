a762831c27a890047257f07d9bd23872d4b853a9
Rosalie Newman <rosalienewman0@gmail.com>
2022-12-02 16:22:30 -0500

    commented out if user already logged in so i can display form, for now

--- src/components/CreateAccountForm.jsx
+++ src/components/CreateAccountForm.jsx
@@ -61,122 +61,122 @@ function CreateAccountForm() {
 	});
 
 	// If the user is already logged in, they should not be able to make an account.
-	if (user)
-		return (
-			<p>
-				You cannot create a new account if you are already logged in. Do you
-				want to log out first?
-			</p>
-		);
+	// if (user)
+	// 	return (
+	// 		<p>
+	// 			You cannot create a new account if you are already logged in. Do you
+	// 			want to log out first?
+	// 		</p>
+	// 	);
+
 	// If no user in slice, give them the form
-	else
-		return (
-			<div>
-				<h2>Create an Account</h2>
-				<h4>
-					Create an account and take advantage of a faster checkout and other
-					benefits.
-				</h4>
+	return (
+		<div>
+			<h2>Create an Account</h2>
+			<h4>
+				Create an account and take advantage of a faster checkout and other
+				benefits.
+			</h4>
 
-				<Box component="form" onSubmit={formik.handleSubmit}>
-					<Box margin={1}>
-						<TextField
-							name="username"
-							type="username"
-							label="Username*"
-							variant="outlined"
-							fullWidth
-							value={formik.values.username}
-							onBlur={formik.handleBlur}
-							onChange={formik.handleChange}
-							error={formik.touched.username && Boolean(formik.errors.username)}
-							helperText={formik.touched.username && formik.errors.username}
-						/>
-					</Box>
-
-					<Box margin={1}>
-						<TextField
-							name="email"
-							type="email"
-							label="Email*"
-							fullWidth
-							variant="outlined"
-							value={formik.values.email}
-							onChange={formik.handleChange}
-							onBlur={formik.handleBlur}
-							error={formik.touched.email && Boolean(formik.errors.email)}
-							helperText={formik.touched.email && formik.errors.email}
-						/>
-					</Box>
-
-					<Box margin={1}>
-						<TextField
-							name="password"
-							type="password"
-							label="Password*"
-							variant="outlined"
-							fullWidth
-							value={formik.values.password}
-							onChange={formik.handleChange}
-							onBlur={formik.handleBlur}
-							error={formik.touched.password && formik.errors.password}
-							helperText={formik.touched.password && formik.errors.password}
-						/>
-					</Box>
-
-					<Box margin={1}>
-						<TextField
-							name="confirmPassword"
-							type="password"
-							label="Confirm Password*"
-							variant="outlined"
-							fullWidth
-							value={formik.values.confirmPassword}
-							onChange={formik.handleChange}
-							onBlur={formik.handleBlur}
-							error={
-								formik.touched.confirmPassword && formik.errors.confirmPassword
-							}
-							helperText={
-								formik.touched.confirmPassword && formik.errors.confirmPassword
-							}
-						/>
-					</Box>
-
-					<Box margin={1}>
-						<TextField
-							name="firstName"
-							type="firstName"
-							label="First Name"
-							variant="outlined"
-							fullWidth
-							value={formik.values.firstName}
-							onChange={formik.handleChange}
-							onBlur={formik.handleBlur}
-							helperText={formik.touched.firstName && formik.errors.firstName}
-						/>
-					</Box>
-
-					<Box margin={1}>
-						<TextField
-							name="lastName"
-							type="lastName"
-							label="Last Name"
-							variant="outlined"
-							fullWidth
-							value={formik.values.lastName}
-							onChange={formik.handleChange}
-							onBlur={formik.handleBlur}
-							helperText={formik.touched.lastName && formik.errors.lastName}
-						/>
-					</Box>
-
-					<Button type="submit" variant="contained">
-						Submit
-					</Button>
+			<Box component="form" onSubmit={formik.handleSubmit}>
+				<Box margin={1}>
+					<TextField
+						name="username"
+						type="username"
+						label="Username*"
+						variant="outlined"
+						fullWidth
+						value={formik.values.username}
+						onBlur={formik.handleBlur}
+						onChange={formik.handleChange}
+						error={formik.touched.username && Boolean(formik.errors.username)}
+						helperText={formik.touched.username && formik.errors.username}
+					/>
 				</Box>
-			</div>
-		);
+
+				<Box margin={1}>
+					<TextField
+						name="email"
+						type="email"
+						label="Email*"
+						fullWidth
+						variant="outlined"
+						value={formik.values.email}
+						onChange={formik.handleChange}
+						onBlur={formik.handleBlur}
+						error={formik.touched.email && Boolean(formik.errors.email)}
+						helperText={formik.touched.email && formik.errors.email}
+					/>
+				</Box>
+
+				<Box margin={1}>
+					<TextField
+						name="password"
+						type="password"
+						label="Password*"
+						variant="outlined"
+						fullWidth
+						value={formik.values.password}
+						onChange={formik.handleChange}
+						onBlur={formik.handleBlur}
+						error={formik.touched.password && formik.errors.password}
+						helperText={formik.touched.password && formik.errors.password}
+					/>
+				</Box>
+
+				<Box margin={1}>
+					<TextField
+						name="confirmPassword"
+						type="password"
+						label="Confirm Password*"
+						variant="outlined"
+						fullWidth
+						value={formik.values.confirmPassword}
+						onChange={formik.handleChange}
+						onBlur={formik.handleBlur}
+						error={
+							formik.touched.confirmPassword && formik.errors.confirmPassword
+						}
+						helperText={
+							formik.touched.confirmPassword && formik.errors.confirmPassword
+						}
+					/>
+				</Box>
+
+				<Box margin={1}>
+					<TextField
+						name="firstName"
+						type="firstName"
+						label="First Name"
+						variant="outlined"
+						fullWidth
+						value={formik.values.firstName}
+						onChange={formik.handleChange}
+						onBlur={formik.handleBlur}
+						helperText={formik.touched.firstName && formik.errors.firstName}
+					/>
+				</Box>
+
+				<Box margin={1}>
+					<TextField
+						name="lastName"
+						type="lastName"
+						label="Last Name"
+						variant="outlined"
+						fullWidth
+						value={formik.values.lastName}
+						onChange={formik.handleChange}
+						onBlur={formik.handleBlur}
+						helperText={formik.touched.lastName && formik.errors.lastName}
+					/>
+				</Box>
+
+				<Button type="submit" variant="contained">
+					Submit
+				</Button>
+			</Box>
+		</div>
+	);
 }
 
 export default CreateAccountForm;
