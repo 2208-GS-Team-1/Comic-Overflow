e8bec859e728e0ea55f018390675dde9c90d9a8e
Rosalie Newman <rosalienewman0@gmail.com>
2022-12-02 16:34:12 -0500

    put catch all in express so refreshing browser page and trying to navigate around via url bar works.

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const volleyball = require('volleyball');
 
 // Set up middleware
-app.use(volleyball)
+app.use(volleyball);
 app.use(express.json());
 
 // Set up routes
@@ -13,6 +13,8 @@ app.use('/api', require('./api'));
 // Pass back everything else / front-end
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
 app.use('/static', express.static(path.join(__dirname, '../static')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../static/index.html')));
+app.get('/*', (req, res) =>
+	res.sendFile(path.join(__dirname, '../static/index.html'))
+);
 
 module.exports = app;
