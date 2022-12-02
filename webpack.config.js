0da863c81b4dd1f7243877d58dc121d6dcf2bfe4
Rosalie Newman <rosalienewman0@gmail.com>
2022-12-02 18:19:35 -0500

    hopefully editted my spacing

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-	devtool: 'source-map',
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				options: {
-					presets: ['@babel/preset-env', '@babel/preset-react'],
-				},
-			},
-		],
-	},
-	resolve: {
-		extensions: ['*', '.js', '.jsx'],
-	},
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
 };
