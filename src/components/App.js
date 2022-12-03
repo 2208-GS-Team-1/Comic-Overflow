2632acf7abb6d65760137453e698a237bae71d64
Yijin Ro <yri.carmine@gmail.com>
2022-12-03 13:31:59 -0500

    added single product route

--- src/components/App.js
+++ src/components/App.js
@@ -6,31 +6,33 @@ import Books from './Books.jsx';
 import Account from './Account.jsx';
 import Login from './Login.jsx';
 import CreateAccountContainer from './createAccount/CreateAccountContainer.jsx';
+import SingleProduct from './SingleProduct/SingleProduct.jsx';
 
 const App = () => {
 	return (
-		<div>
-			<div className="main_header">
-				<h1>Comic Overflow</h1>
-			</div>
+    <div>
+      <div className="main_header">
+        <h1>Comic Overflow</h1>
+      </div>
 
-			<div>
-				<nav className="navbar">
-					<Link to="/">Home</Link>
-					<Link to="/books">Books</Link>
-					<Link to="/login">Log-In</Link>
-					<Link to="/createaccount">Create Account</Link>
-				</nav>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/books" element={<Books />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/createaccount" element={<CreateAccountContainer />} />
-					<Route path="/account" element={<Account />} />
-				</Routes>
-			</div>
-		</div>
-	);
+      <div>
+        <nav className="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/books">Books</Link>
+          <Link to="/login">Log-In</Link>
+          <Link to="/createaccount">Create Account</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/createaccount" element={<CreateAccountContainer />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/books/:id" element={<SingleProduct />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
