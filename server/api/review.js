cc010870fb40b30bf5ac17321932b2631045df15
Ed McCormick <edward.mccormick3@gmail.com>
2022-12-03 19:39:06 -0600

    completed review seeding and simple get route for single book

--- server/api/review.js
+++ server/api/review.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { Review } = require("../db");
 
-// GET - api/review/:id --> Gets all reviews from the db
-router.get("/:id", async (req, res, next) => {
-  const id = req.params.id;
+// GET - api/review/:bookId --> Gets all reviews for a specific book from the db
+router.get("/:bookId", async (req, res, next) => {
+  const id = +req.params.bookId;
   try {
-    const userReviews = await Review.findByPk(id);
+    const userReviews = await Review.findAll({
+      where: { bookId: id },
+    });
     res.send(userReviews);
   } catch (err) {
     console.log(err);
