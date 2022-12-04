5fd703a0afc63255beb2082069c25175b152fd84
Ed McCormick <edward.mccormick3@gmail.com>
2022-12-04 11:25:59 -0600

    ratings update

--- src/components/Ratings/StarRatingAvg.jsx
+++ src/components/Ratings/StarRatingAvg.jsx
@@ -2,46 +2,42 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const StarRatingAvg = ({ book }) => {
+  //book gets passed down as prop from AllBooks.jsx
   //use local state
   const [allReviews, setAllReviews] = useState([]);
+  //const [avgReview, setAvgReview] = useState(0);
   const [loading, setLoading] = useState(false);
-  //initialize review array variable
-  //book gets passed down as prop from AllBooks.jsx
+  //initialize average review variable
+  let avgReview;
   const handleAllReviews = async () => {
     setLoading(true);
     // GET all reviews for specific book
     const reviews = await axios.get(`api/reviews/${book.id}`);
     //reviews.data = array of reivews for specific book
     setAllReviews(reviews.data);
-    console.log(allReviews.length);
+
     setLoading(false);
+    console.log("REVIEWS" + allReviews);
   };
+
   //call function to pass reviews to allReviews
   useEffect(() => {
     handleAllReviews();
+    // handleAvgReviews();
   }, []);
-
   //if length of array > 1 then take average of rating
   if (loading) return <h1>Loading...</h1>;
   return <div>{allReviews.length ? "this has ratings" : "no ratings"}</div>;
 };
 export default StarRatingAvg;
 
-// const StarRatingAvg = ({ book }) => {
-//   //initialize review array variable
-//   let allReviews = [];
-//   //book gets passed down as prop from AllBooks.jsx
-//   const getAllRatings = async () => {
-//     // GET all reviews for specific book
-//     const reviews = await axios.get(`api/reviews/${book.id}`);
-//     //reviews.data = array of reivews for specific book
-//     allReviews = reviews.data;
-//     console.log(allReviews.length);
-//     return;
+//   const handleAvgReviews = () => {
+//     setLoading(true);
+//     if (allReviews.length > 0) {
+//       setAvgReview(
+//         allReviews.reduce((total, next) => total + next.rating, 0) /
+//           allReviews.length
+//       );
+//     }
+//     setLoading(false);
 //   };
-//   //call function to pass reviews to allReviews
-//   getAllRatings();
-
-//   //if length of array > 1 then take average of rating
-//   return <div>{allReviews.length ? "this has ratings" : "no ratings"}</div>;
-// };
