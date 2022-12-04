00e5320ed611247b40a6d8cce45290eb21b96c75
Ed McCormick <edward.mccormick3@gmail.com>
2022-12-04 09:07:01 -0600

    need to add local state

--- src/components/Ratings/StarRatingAvg.jsx
+++ src/components/Ratings/StarRatingAvg.jsx
@@ -2,12 +2,22 @@ import axios from "axios";
 import React from "react";
 
 const StarRatingAvg = ({ book }) => {
+  //initialize review array variable
+  let allReviews = [];
+  //book gets passed down as prop from AllBooks.jsx
   const getAllRatings = async () => {
+    // GET all reviews for specific book
     const reviews = await axios.get(`api/reviews/${book.id}`);
-    console.log(reviews.data);
+    //reviews.data = array of reivews for specific book
+    allReviews = reviews.data;
+    console.log(allReviews.length);
+    return;
   };
+  //call function to pass reviews to allReviews
   getAllRatings();
-  return <div>{book.id}</div>;
+
+  //if length of array > 1 then take average of rating
+  return <div>{allReviews.length ? "this has ratings" : "no ratings"}</div>;
 };
 
 export default StarRatingAvg;
