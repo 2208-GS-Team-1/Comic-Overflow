247bed4832e77701a5e0b42a446c5e0a86ce1fa8
Ed McCormick <edward.mccormick3@gmail.com>
2022-12-04 09:31:18 -0600

    added loading state

--- src/components/Ratings/StarRatingAvg.jsx
+++ src/components/Ratings/StarRatingAvg.jsx
@@ -1,23 +1,47 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const StarRatingAvg = ({ book }) => {
+  //use local state
+  const [allReviews, setAllReviews] = useState([]);
+  const [loading, setLoading] = useState(false);
   //initialize review array variable
-  let allReviews = [];
   //book gets passed down as prop from AllBooks.jsx
-  const getAllRatings = async () => {
+  const handleAllReviews = async () => {
+    setLoading(true);
     // GET all reviews for specific book
     const reviews = await axios.get(`api/reviews/${book.id}`);
     //reviews.data = array of reivews for specific book
-    allReviews = reviews.data;
+    setAllReviews(reviews.data);
     console.log(allReviews.length);
-    return;
+    setLoading(false);
   };
   //call function to pass reviews to allReviews
-  getAllRatings();
+  useEffect(() => {
+    handleAllReviews();
+  }, []);
 
   //if length of array > 1 then take average of rating
+  if (loading) return <h1>Loading...</h1>;
   return <div>{allReviews.length ? "this has ratings" : "no ratings"}</div>;
 };
-
 export default StarRatingAvg;
+
+// const StarRatingAvg = ({ book }) => {
+//   //initialize review array variable
+//   let allReviews = [];
+//   //book gets passed down as prop from AllBooks.jsx
+//   const getAllRatings = async () => {
+//     // GET all reviews for specific book
+//     const reviews = await axios.get(`api/reviews/${book.id}`);
+//     //reviews.data = array of reivews for specific book
+//     allReviews = reviews.data;
+//     console.log(allReviews.length);
+//     return;
+//   };
+//   //call function to pass reviews to allReviews
+//   getAllRatings();
+
+//   //if length of array > 1 then take average of rating
+//   return <div>{allReviews.length ? "this has ratings" : "no ratings"}</div>;
+// };
