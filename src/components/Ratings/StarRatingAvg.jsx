eeddbc64cd6b8391612382bc7204ea3321347c9e
Ed McCormick <edward.mccormick3@gmail.com>
2022-12-04 11:36:10 -0600

    deleted commented code

--- src/components/Ratings/StarRatingAvg.jsx
+++ src/components/Ratings/StarRatingAvg.jsx
@@ -31,38 +31,3 @@ const StarRatingAvg = ({ book }) => {
   return <div>{avgReview > 0 ? avgReview : "no ratings"}</div>;
 };
 export default StarRatingAvg;
-
-// const StarRatingAvg = ({ book }) => {
-//     //book gets passed down as prop from AllBooks.jsx
-//     //use local state
-//     const [allReviews, setAllReviews] = useState([]);
-//     const [avgReview, setAvgReview] = useState(0);
-//     const [loading, setLoading] = useState(false);
-
-//     const handleAllReviews = async () => {
-//       setLoading(true);
-//       // GET all reviews for specific book
-//       const reviews = await axios.get(`api/reviews/${book.id}`);
-//       //reviews.data = array of reivews for specific book
-//       setAllReviews(reviews.data);
-
-//       setLoading(false);
-//       console.log("REVIEWS" + allReviews);
-//     };
-
-//         if (allReviews.length > 0) {
-//           setAvgReview(
-//             allReviews.reduce((total, next) => total + next.rating, 0) /
-//               allReviews.length
-//           );
-//         }
-//     //call function to pass reviews to allReviews
-//     useEffect(() => {
-//       handleAllReviews();
-//       // handleAvgReviews();
-//     }, []);
-//     //if length of array > 1 then take average of rating
-//     if (loading) return <h1>Loading...</h1>;
-//     return <div>{allReviews.length ? "this has ratings" : "no ratings"}</div>;
-//   };
-//   export default StarRatingAvg;
