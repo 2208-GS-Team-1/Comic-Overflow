14def19814d812687e4bed55545ff1aabf3774a6
Ed McCormick <edward.mccormick3@gmail.com>
2022-12-03 20:49:49 -0600

    pluralized reviews to match formatting

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 router.use("/auth", require("./auth"));
 router.use("/books", require("./books"));
 router.use("/users", require("./users"));
-router.use("/review", require("./review"));
+router.use("/reviews", require("./reviews"));
 
 module.exports = router;
