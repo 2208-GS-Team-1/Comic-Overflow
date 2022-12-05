7e892a51ad57734cc30a7669911796eca3e94cbb
Rosalie Newman <rosalienewman0@gmail.com>
2022-12-05 14:10:56 -0500

    active carts are seeded to reflect my excalidraw drawing. Have not created roses previous purchases seed.

--- server/db/dbSeeds/cartSeed.js
+++ server/db/dbSeeds/cartSeed.js
@@ -4,32 +4,29 @@ const cartSeed = async (books, users) => {
   //console.log(`Magic methods are in here: ${Object.keys(CartItem.prototype)}`);
 
   // Destruct users out of users object
-  const { moe, lucy, larry, ethyl } = users;
+  const { moe, lucy, larry, ethyl, rose } = users;
 
   // Destruct books out of book object
   const { devilman1, adamStrange1, wonderWoman, silverSurfer, xmen } = books;
 
   // Below are various ways of creating cart items & their associations:
 
-  //*********************** Moe has devilman volume 1 in his cart ************************//
+  //*********************** MOE CART SEEDING ************************//
 
+  // Moe has devilman volume 1 in his cart
   // Creating the instance with explicitly giving it the ids:
   const moesFirstCartItem = await CartItem.create({
     userId: moe.id,
     bookId: devilman1.id,
   });
 
-  //*********************** Moe has wonder woman volume 89 in his cart ************************//
-
+  // Moe has wonder woman volume 89 in his cart
   // This creates the cartitem instance with no associations yet:
   const moesSecondCartItem = await CartItem.create({});
 
   // Using some magic methods to make associations
   await moe.addCartItem(moesSecondCartItem);
   await wonderWoman.addCartItem(moesSecondCartItem);
-
-  // Here are some ways to in
-  // await moe.addCartItem(moesFirstCartItem);
 
   // Now to test if our MOE entries are right, by requerying for them with joining.
   let testCartItem = await CartItem.findByPk(1, {
@@ -46,9 +43,11 @@ const cartSeed = async (books, users) => {
   its checked out status is: ${testCartItem.isCheckedOut}
   and the book is ${testCartItem.book.title} volume ${testCartItem.book.volume}\n`);
 
-  //*********************** Lucy has already purchased Wonder Woman vol.89 ************************//
+  //*********************** LUCY CART SEEDING ************************//
 
-  // Let's say when lucy bought this, it was at a different price than what we currently charge, to test this feature.
+  // Lucy has already purchased Wonder Woman vol.89
+  // Let's say when lucy bought this, it was at a different price than what we currently charge,
+  // to test this feature.
   const lucyFirstCartItem = await CartItem.create({
     userId: lucy.id,
     bookId: wonderWoman.id,
@@ -58,15 +57,13 @@ const cartSeed = async (books, users) => {
     timeOfCheckOut: new Date(),
   });
 
-  //*********************** Lucy has Adam Strange in her cart. ************************//
+  // Lucy has Adam Strange in her cart.
   const lucySecondCartItem = await CartItem.create({
     userId: lucy.id,
     bookId: adamStrange1.id,
   });
 
-  //*********************** Lucy has Wonder Woman in her cart (she wants another copy). ************************//
-
-  // She wants another copy, but has not bought it yet.
+  // Lucy has Wonder Woman in her cart (she wants another copy)
   const lucyThirdCartItem = await CartItem.create({
     userId: lucy.id,
     bookId: wonderWoman.id,
@@ -82,10 +79,48 @@ const cartSeed = async (books, users) => {
   console.log(
     `${boughtWW.user.firstName} previously purchased ${boughtWW.book.title} vol.${boughtWW.book.volume} for ${boughtWW.priceAtCheckOut}. 
     Its checkout status is ${boughtWW.isCheckedOut} and orderStatus is ${boughtWW.orderStatus}.
-    She bought it at: ${boughtWW.timeOfCheckout}
+    She bought it at: ${boughtWW.timeOfCheckOut}
     But she wants to buy it again, and the price we are selling it for now
     is: ${wonderWoman.price}, therefore the item in her cart is: ${inCartWW.book.price}.`
   );
+
+  //*********************** LARRY'S CART SEEDING ************************//
+  // Larry's Cart is:
+  // Berserk vol.1,         quatity: 1
+  // Berserk vol.2,         quatity: 2
+  //           (quantity handled by just making 2 cart items, quantity calculated on front end)
+  // Berserk vol.1 DELUXE edition, quatity: 1
+  await Promise.all([
+    CartItem.create({
+      userId: larry.id,
+      bookId: books.berserk1.id,
+    }),
+    CartItem.create({
+      userId: larry.id,
+      bookId: books.berserk2.id,
+    }),
+    CartItem.create({
+      userId: larry.id,
+      bookId: books.berserk2.id,
+    }),
+    CartItem.create({
+      userId: larry.id,
+      bookId: books.berserkDeluxe1.id,
+    }),
+  ]);
+
+  //*********************** ETHYL'S CART SEEDING ************************//
+  // Ethyl has Akagi vol 36 in her cart, which is actually out of stock.
+  // This is valid, we will have to decide when to remove it from a user's cart
+  // EG if they try to check out,
+  // remove it first (front-end calls DELETE /api/cart/:cartItem)
+  await CartItem.create({
+    userId: ethyl.id,
+    bookId: books.akagi36.id,
+  });
+
+  //*********************** ROSE'S CART SEEDING ************************//
+  // Active Cart is empty
 
   return;
 };
