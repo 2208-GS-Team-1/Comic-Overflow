f8e370d04c17412ebd38bb158517998fd0ca8dee
Joshowens16 <joshowens16@gmail.com>
2022-12-05 09:19:57 -0800

    added UUID REGEX test to users API

--- server/api/users.js
+++ server/api/users.js
@@ -14,15 +14,22 @@ router.get("/", async (req, res, next) => {
 // GET - api/users/:id --> Gets all users from the db
 router.get("/:id", async (req, res, next) => {
   const id = req.params.id;
-  try {
-    const singleUser = await User.findOne({
-      where: {
-        id: id,
-      },
-    });
-    res.send(singleUser);
-  } catch (err) {
-    next(err);
+  const regexExp =
+    /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+  const isUUID = regexExp.test(id);
+  if (isUUID) {
+    try {
+      const singleUser = await User.findByPk(id);
+      if (singleUser) {
+        res.send(singleUser);
+      } else {
+        res.sendStatus(404);
+      }
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    res.send("ID is not a UUID").status(404);
   }
 });
 // POST - api/users --> Adds user to db
