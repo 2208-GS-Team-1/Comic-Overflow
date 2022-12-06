cb0445a57c4231fa9c9db16df10b947dda32205f
Rosalie Newman <rosalienewman0@gmail.com>
2022-12-06 17:17:03 -0500

    adminHomepage barebones

--- src/components/admin/AdminHomepage.jsx
+++ src/components/admin/AdminHomepage.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 function AdminHomepage(props) {
-  const user = useSelector(state => state.user);
-  return <div>Todo</div>;
+  const { user } = useSelector(state => state.user);
+  console.log(user);
+  console.log(`user.isAdmin: ${user.isAdmin}`);
 
-  // console.log(user.isAdmin);
-  // if (!user.isAdmin) return <h2>You are not an admin, permission denied</h2>;
-  // return <div>Welcome admin!</div>;
+  if (!user.isAdmin) return <h2>You are not an admin, permission denied</h2>;
+  return (
+    <div>
+      <h1>Welcome admin!</h1>
+      <p>What would you like to interact with</p>
+      <button>Users</button>
+      <button>Books</button>
+    </div>
+  );
 }
 
 export default AdminHomepage;
