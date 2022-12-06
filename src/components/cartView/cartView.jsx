b6df8166b5581b02b4c955432bd5dee2d33c1b36
Joshowens16 <joshowens16@gmail.com>
2022-12-06 11:49:19 -0800

    basic cart slice, no delete or add just set to set up basic cart view

---
+++ src/components/cartView/cartView.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const cartView = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default cartView;
---
+++ src/store/cartSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    setCart: (state, action) => {
+      state.cart = action.payload;
+    },
+  },
+});
+
+export const { setBooks } = cartSlice.actions;
+export default cartSlice.reducer;
