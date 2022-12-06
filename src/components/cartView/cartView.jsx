942a0ffd293be336f1add9d9c2d5e701020167be
Joshowens16 <joshowens16@gmail.com>
2022-12-06 11:51:39 -0800

    cart reducer semi done

--- src/components/cartView/cartView.jsx
+++ src/components/cartView/cartView.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const cartView = () => {
+    const cart = useSelector(state => state.cart.cart)
     return (
         <div>
             
