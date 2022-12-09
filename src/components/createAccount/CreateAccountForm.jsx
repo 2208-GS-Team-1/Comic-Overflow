3ba9d00a701ddb54d85a66b91ae04cb5b4757b4a
Rosalie Newman <rosalienewman0@gmail.com>
2022-12-09 18:40:27 -0500

    began handling for if username or email already exists in db to render a helpful message to user

--- src/components/createAccount/CreateAccountForm.jsx
+++ src/components/createAccount/CreateAccountForm.jsx
@@ -1,165 +1,200 @@
-import React from 'react';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-import { Box, Button, TextField } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { Alert, Box, Button, TextField } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 // Validation schema using yup, to check is text field entries are valid.
 const validationSchema = yup.object({
-	email: yup
-		.string('Enter your email')
-		.email('Enter a valid email')
-		.required('Email is required'),
-	password: yup
-		.string('Enter your password')
-		.min(3, 'Password should be a minimum 3 characters')
-		.required('Password is required'),
-	confirmPassword: yup
-		.string('Confirm password')
-		// This is how you look at 'password' and make sure it is the same:
-		.oneOf([yup.ref('password'), null], 'Passwords should match'),
-	firstName: yup.string('Enter your first name'),
-	lastName: yup.string('Enter your last name'),
+  email: yup
+    .string("Enter your email")
+    .email("Enter a valid email")
+    .required("Email is required"),
+  password: yup
+    .string("Enter your password")
+    .min(3, "Password should be a minimum 3 characters")
+    .required("Password is required"),
+  confirmPassword: yup
+    .string("Confirm password")
+    // This is how you look at 'password' and make sure it is the same:
+    .oneOf([yup.ref("password"), null], "Passwords should match"),
+  firstName: yup.string("Enter your first name"),
+  lastName: yup.string("Enter your last name"),
 });
 
 function CreateAccountForm() {
-	// Redux and React stuff
-	const navigate = useNavigate();
+  // Redux and React stuff
+  const navigate = useNavigate();
 
-	const formik = useFormik({
-		// Initializes our formik.values object to have these key-value pairs.
-		initialValues: {
-			email: '',
-			password: '',
-			firstName: '',
-			lastName: '',
-		},
-		// Give it our yup validationSchema
-		validationSchema: validationSchema,
-		onSubmit: async values => {
-			console.log(values);
-			// Update Backend: axios post req to User
-			//const createdUser = await axios.post('api/user', values);
-			// Update Slice?
-			//dispatch(setUser());
-			alert('Account created!');
-			navigate('/'); // Redirect user to homepage
-		},
-	});
+  // States used for error message display rendering if account creation was successful or not
+  const [creationSuccess, setCreationSuccess] = useState(false);
+  const [creationFailure, setCreationFailure] = useState(false);
 
-	return (
-		<div>
-			<Box
-				component="form"
-				onSubmit={formik.handleSubmit}
-				sx={{
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-					justifyContent: 'space-between',
-				}}
-			>
-				<Box margin={1}>
-					<TextField
-						name="username"
-						type="username"
-						label="Username*"
-						variant="outlined"
-						fullWidth
-						value={formik.values.username || ''}
-						onBlur={formik.handleBlur}
-						onChange={formik.handleChange}
-						error={formik.touched.username && Boolean(formik.errors.username)}
-						helperText={formik.touched.username && formik.errors.username}
-					/>
-				</Box>
+  const formik = useFormik({
+    // Initializes our formik.values object to have these key-value pairs.
+    initialValues: {
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+    },
+    // Give it our yup validationSchema
+    validationSchema: validationSchema,
+    onSubmit: async values => {
+      console.log(values);
+      // Update Backend: axios post req to User
+      try {
+        await axios.post("/api/users", values);
+        /* Ideally would like to log the user in here, and THEN redirect to home
+      	But not sure how to generate the JWT and the stuff with authorization.
+				Would also need to dispatch the redux state of user, I think.
 
-				<Box margin={1}>
-					<TextField
-						name="email"
-						type="email"
-						label="Email*"
-						fullWidth
-						variant="outlined"
-						value={formik.values.email || ''}
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-						error={formik.touched.email && Boolean(formik.errors.email)}
-						helperText={formik.touched.email && formik.errors.email}
-					/>
-				</Box>
+				So for now, just redirect them to the login page so they can log in with new account.
+			  */
 
-				<Box margin={1}>
-					<TextField
-						name="password"
-						type="password"
-						label="Password*"
-						variant="outlined"
-						fullWidth
-						value={formik.values.password || ''}
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-						error={formik.touched.password && Boolean(formik.errors.password)}
-						helperText={formik.touched.password && formik.errors.password}
-					/>
-				</Box>
+        // Update the state that is used to trigger the mui Alert Success component
+        setCreationSuccess(true);
+        // In case it failed before, set that state too, so the failure message disappears
+        setCreationFailure(false);
 
-				<Box margin={1}>
-					<TextField
-						name="confirmPassword"
-						type="password"
-						label="Confirm Password*"
-						variant="outlined"
-						fullWidth
-						value={formik.values.confirmPassword || ''}
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-						error={
-							formik.touched.confirmPassword &&
-							Boolean(formik.errors.confirmPassword)
-						}
-						helperText={
-							formik.touched.confirmPassword && formik.errors.confirmPassword
-						}
-					/>
-				</Box>
+        // Wait 3 seconds before redirecting the user to login page
+        setTimeout(() => {
+          navigate("/login");
+        }, 2000);
+      } catch (err) {
+        // If account creation failed (eg, an acct with that username already exists)
+        setCreationFailure(true);
+      }
+    },
+  });
 
-				<Box margin={1}>
-					<TextField
-						name="firstName"
-						type="firstName"
-						label="First Name"
-						variant="outlined"
-						fullWidth
-						value={formik.values.firstName || ''}
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-						error={formik.touched.firstName && Boolean(formik.errors.firstName)}
-						helperText={formik.touched.firstName && formik.errors.firstName}
-					/>
-				</Box>
+  return (
+    <div>
+      <Box
+        component="form"
+        onSubmit={formik.handleSubmit}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box margin={1}>
+          <TextField
+            name="username"
+            type="username"
+            label="Username*"
+            variant="outlined"
+            fullWidth
+            value={formik.values.username || ""}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            error={formik.touched.username && Boolean(formik.errors.username)}
+            helperText={formik.touched.username && formik.errors.username}
+          />
+        </Box>
 
-				<Box margin={1}>
-					<TextField
-						name="lastName"
-						type="lastName"
-						label="Last Name"
-						variant="outlined"
-						fullWidth
-						value={formik.values.lastName || ''}
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-						error={formik.touched.lastName && Boolean(formik.errors.lastName)}
-						helperText={formik.touched.lastName && formik.errors.lastName}
-					/>
-				</Box>
+        <Box margin={1}>
+          <TextField
+            name="email"
+            type="email"
+            label="Email*"
+            fullWidth
+            variant="outlined"
+            value={formik.values.email || ""}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+          />
+        </Box>
 
-				<Button type="submit" sx={{ width: 200 }} variant="contained">
-					Submit
-				</Button>
-			</Box>
-		</div>
-	);
+        <Box margin={1}>
+          <TextField
+            name="password"
+            type="password"
+            label="Password*"
+            variant="outlined"
+            fullWidth
+            value={formik.values.password || ""}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+          />
+        </Box>
+
+        <Box margin={1}>
+          <TextField
+            name="confirmPassword"
+            type="password"
+            label="Confirm Password*"
+            variant="outlined"
+            fullWidth
+            value={formik.values.confirmPassword || ""}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={
+              formik.touched.confirmPassword &&
+              Boolean(formik.errors.confirmPassword)
+            }
+            helperText={
+              formik.touched.confirmPassword && formik.errors.confirmPassword
+            }
+          />
+        </Box>
+
+        <Box margin={1}>
+          <TextField
+            name="firstName"
+            type="firstName"
+            label="First Name"
+            variant="outlined"
+            fullWidth
+            value={formik.values.firstName || ""}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.firstName && Boolean(formik.errors.firstName)}
+            helperText={formik.touched.firstName && formik.errors.firstName}
+          />
+        </Box>
+
+        <Box margin={1}>
+          <TextField
+            name="lastName"
+            type="lastName"
+            label="Last Name"
+            variant="outlined"
+            fullWidth
+            value={formik.values.lastName || ""}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+            helperText={formik.touched.lastName && formik.errors.lastName}
+          />
+        </Box>
+
+        <Button type="submit" sx={{ width: "300px" }} variant="contained">
+          Submit
+        </Button>
+
+        {creationSuccess && (
+          <Alert sx={{ marginTop: 2 }} severity="success">
+            Account created! Redirecting to login page...
+          </Alert>
+        )}
+
+        {creationFailure && (
+          <Alert sx={{ marginTop: 2 }} severity="error">
+            An account with that username or email already exists
+          </Alert>
+        )}
+      </Box>
+    </div>
+  );
 }
 
 export default CreateAccountForm;
