74e08d782e783a3e4547d1b5f0d6e9a1611e3382
Rosalie Newman <rosalienewman0@gmail.com>
2022-12-09 00:56:33 -0500

    deleted an extra curly from a component i didnt even touch, no idea why git merge origin main did that

--- src/components/addToCart/AllProductsAdd.jsx
+++ src/components/addToCart/AllProductsAdd.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { Button } from '@mui/material';
-import { AddShoppingCart } from '@mui/icons-material';
-import { useSelector } from 'react-redux';
-import axios from 'axios';
-const allProductsAdd =  ({ bookId }) => {
-    const { user } = useSelector((state) => state.user);
-    const userId = user.id
-    const addToCart = async () => {
-        if(!user.id){
-            alert('Please login to add to cart')
-        } else {
-            const body = {userId, bookId}
-            await axios.post('/api/cart', body)
-            alert(`${user.firstName}'s cart was updated!`)
-        }
+import React from "react";
+import { Button } from "@mui/material";
+import { AddShoppingCart } from "@mui/icons-material";
+import { useSelector } from "react-redux";
+import axios from "axios";
+const allProductsAdd = ({ bookId }) => {
+  const { user } = useSelector(state => state.user);
+  const userId = user.id;
+  const addToCart = async () => {
+    if (!user.id) {
+      alert("Please login to add to cart");
+    } else {
+      const body = { userId, bookId };
+      await axios.post("/api/cart", body);
+      alert(`${user.firstName}'s cart was updated!`);
     }
   };
   return (
