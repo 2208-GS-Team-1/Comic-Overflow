bd971988a3627cee76dd1c3d340471faf7b75bee
Ed McCormick <edward.mccormick3@gmail.com>
2022-12-09 21:24:13 -0600

    added local storage

--- src/components/Books/AllBooks.jsx
+++ src/components/Books/AllBooks.jsx
@@ -1,5 +1,11 @@
-
-import { Button, Card, CardActions, CircularProgress, LinearProgress, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CircularProgress,
+  LinearProgress,
+  Typography,
+} from "@mui/material";
 import { sizing } from "@mui/system";
 import { Box } from "@mui/system";
 
@@ -16,8 +22,7 @@ import { Link } from "react-router-dom";
 import "./books.css";
 
 import StarRatingAvg from "../Reviews/StarRatingAvg";
-import AllProductsAdd from "../addToCart/AllProductsAdd"
-
+import AllProductsAdd from "../addToCart/AllProductsAdd";
 
 const AllBooks = () => {
   const dispatch = useDispatch();
@@ -36,43 +41,40 @@ const AllBooks = () => {
   useEffect(() => {
     fetchBooks();
   }, []);
-  if (loading) return(
-    <div
-    className="loadingContainer">
-      <MuiLoader/>
-    </div>
-  )
+  if (loading)
+    return (
+      <div className="loadingContainer">
+        <MuiLoader />
+      </div>
+    );
   return (
     <div className="productsContainer">
       <h1>All Comics</h1>
 
-    <div className="allBooks">
-      {books.map((book) => {
-        return (
-          <Card
-            sx={{ boxShadow: 2 }}
-            className="productCard"
-            variant="outlined"
-            key={book.id}>
-            <div className="productCardImg">
-              <Link to={`/books/${book.id}`}>
-                <img src={book.imageURL} />
-              </Link>
-            </div>
-            <div
-            className="cardRatings"
-            >
-              <StarRatingAvg key={book.id} book={book} />
-            </div>
-            <div className="productCardButtons">
-              <Typography>${(book.price / 100).toFixed(2)}</Typography>
-              <AllProductsAdd bookId={book.id}/>
-            </div>
-          </Card>
-        );
-      })}
-    </div>
-
+      <div className="allBooks">
+        {books.map((book) => {
+          return (
+            <Card
+              sx={{ boxShadow: 2 }}
+              className="productCard"
+              variant="outlined"
+              key={book.id}>
+              <div className="productCardImg">
+                <Link to={`/books/${book.id}`}>
+                  <img src={book.imageURL} />
+                </Link>
+              </div>
+              <div className="cardRatings">
+                <StarRatingAvg key={book.id} book={book} />
+              </div>
+              <div className="productCardButtons">
+                <Typography>${(book.price / 100).toFixed(2)}</Typography>
+                <AllProductsAdd book={book} />
+              </div>
+            </Card>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/components/addToCart/AllProductsAdd.jsx
+++ src/components/addToCart/AllProductsAdd.jsx
@@ -3,13 +3,15 @@ import { Button } from "@mui/material";
 import { AddShoppingCart } from "@mui/icons-material";
 import { useSelector } from "react-redux";
 import axios from "axios";
-const allProductsAdd = ({ bookId }) => {
-  const { user } = useSelector(state => state.user);
+const allProductsAdd = ({ book }) => {
+  const { user } = useSelector((state) => state.user);
   const userId = user.id;
   const addToCart = async () => {
-    if (!user.id) {
-      alert("Please login to add to cart");
+    //if user is not logged in and there is stock, add to local storage
+    if (!user.id && book.stock) {
+      localStorage.setItem(book, JSON.stringify(book));
     } else {
+      const bookId = book.id;
       const body = { userId, bookId };
       await axios.post("/api/cart", body);
       alert(`${user.firstName}'s cart was updated!`);
