c940210cf8b44926b81692c65d98b02da87cb9ed
Joshowens16 <joshowens16@gmail.com>
2022-12-10 15:59:19 -0800

    works from all products add

--- src/components/addToCart/AllProductsAdd.jsx
+++ src/components/addToCart/AllProductsAdd.jsx
@@ -7,8 +7,27 @@ import { setCart } from "../../store/cartSlice";
 const allProductsAdd = ({ bookId }) => {
   const { user } = useSelector(state => state.user);
   const dispatch = useDispatch()
-  const { cart } = useSelector((state) => state.cart);
+//   const { cart } = useSelector((state) => state.cart);
   const userId = user.id;
+  const loadCartFromLocalStorage = () => {
+    // Get the stringified cart from local storage
+    const cartString = localStorage.getItem('cart');
+  
+    // Parse the stringified cart to get the original cart object
+    const cart = JSON.parse(cartString);
+  
+    // Return the cart object
+    return cart;
+  };
+  const saveCartToLocalStorage = cart => {
+    // Local storage can only store strings, so we need to convert the cart object to a string
+    const cartString = JSON.stringify(cart);
+  
+    // Now we can save the stringified cart to local storage
+    localStorage.setItem('cart', cartString);
+  };
+
+  const cart = loadCartFromLocalStorage();
   const addToCart = async () => {
     try {
         if (!user.id) {
@@ -24,11 +43,13 @@ const allProductsAdd = ({ bookId }) => {
                 });
                 const updatedCart = await axios.get(`/api/cart/user/${user.id}`)
                 dispatch(setCart(updatedCart.data))
+                saveCartToLocalStorage(updatedCart.data)
             } else {
                 const body = { userId, bookId };
                 await axios.post("/api/cart", body);
                 const updatedCart = await axios.get(`/api/cart/user/${user.id}`)
                 dispatch(setCart(updatedCart.data))
+                saveCartToLocalStorage(updatedCart.data)
             }
         }
     } catch (error) {
