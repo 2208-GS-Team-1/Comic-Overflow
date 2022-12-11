2c237f027f20086796665c8e57221339e28f69c0
Yijin Ro <yri.carmine@gmail.com>
2022-12-11 13:40:59 -0500

    displaying all registered users with buttons to either view or edit

--- src/components/admin/AdminUsersPage.jsx
+++ src/components/admin/AdminUsersPage.jsx
@@ -1,9 +1,9 @@
-import { TableBody } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setAllUsers } from "../../store/userSlice";
 import AdminNavbar from "./adminNavbar";
+import './admin.css'
 
 function AdminUsersPage() {
   // If they're not an admin don't let them see this component.
@@ -15,10 +15,13 @@ function AdminUsersPage() {
   if (!user.isAdmin) return <h2>You are not an admin, permission denied</h2>;
 
   const allUserHandler = async () =>{
-    const userDataFetched = await axios.get('/api/users')
-    dispatch(setAllUsers(userDataFetched.data));
-    setLoading(true);
-    console.log('data loading completed');
+    try {
+      const {data} = await axios.get('/api/users')
+      dispatch(setAllUsers(data));
+      setLoading(true);
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   useEffect(()=>{
@@ -27,24 +30,37 @@ function AdminUsersPage() {
 
   if(!loading){return (<div>Oops! Something went wrong!</div>)}
 
-  return (
-    <div>
-      <AdminNavbar />
-      <div>All Users List</div>
+    return (
       <div>
-        <table>
-          <tbody>
-            <tr>
-              <td>Lucy</td>
-            </tr>
-            <tr>
-              <td>Moe</td>
-            </tr>
-          </tbody>
-        </table>
+        <AdminNavbar />
+        <div className="adminUserPage">
+        <div>
+          <h1>All Users List</h1>
+        </div>
+        <div className="adminUserContainer">
+          <table className="adminUserTable">
+            <tbody>
+                {allUsers.map( (user, index) => {
+                  return (
+                    <tr className="adminUsertr" key={user.id}>
+                      <td className="adminUsertd">
+                        {index}:  {user.firstName} {user.lastName}
+                      </td>
+                      <td className="adminUserButtons">
+                        {/* below buttons should be replaced with components later */}
+                      <button>View</button>
+                      <button>Edit</button>
+                      </td>
+                    </tr>
+                  )
+                })}
+            </tbody>
+          </table>
+        </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+
 }
 
 export default AdminUsersPage;
