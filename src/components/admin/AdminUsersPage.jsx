950975156c0d3d3d9db7130f7e8514165e104114
Yijin Ro <yri.carmine@gmail.com>
2022-12-11 12:43:48 -0500

    initial commit

--- src/components/admin/AdminUsersPage.jsx
+++ src/components/admin/AdminUsersPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import AdminNavbar from "./adminNavbar";
 
 function AdminUsersPage() {
   // If they're not an admin don't let them see this component.
   const { user } = useSelector(state => state.user);
+  const dispatch = useDispatch();
+
   if (!user.isAdmin) return <h2>You are not an admin, permission denied</h2>;
 
   return (
