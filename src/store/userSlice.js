5230ccda160651e538670f0bf6d6deca636995c4
Yijin Ro <yri.carmine@gmail.com>
2022-12-11 12:42:48 -0500

    alluser state added

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -2,7 +2,8 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     user: {},
-    selectedUser: {}
+    selectedUser: {},
+    allUsers: [],
 }
 
 export const userSlice = createSlice({
@@ -18,8 +19,11 @@ export const userSlice = createSlice({
         setSelectedUser: (state, action) => {
             state.selectedUser = action.payload;
         },
+        setAllUsers: (state, action)=>{
+            state.allUsers = action.payload;
+        }
     }
 })
 
-export const { setUser, resetUser, setSelectedUser } = userSlice.actions;
+export const { setUser, resetUser, setSelectedUser, setAllUsers } = userSlice.actions;
 export default userSlice.reducer;
