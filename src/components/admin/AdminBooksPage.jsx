782d23b589d10b73f6db29e2bf9e6fc8e8422022
Yijin Ro <yri.carmine@gmail.com>
2022-12-11 16:47:19 -0500

    displaying list of all products

--- src/components/admin/AdminBooksPage.jsx
+++ src/components/admin/AdminBooksPage.jsx
@@ -1,22 +1,59 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setBooks } from "../../store/bookSlice";
 import AdminNavbar from "./adminNavbar";
 
 function AdminBooksPage() {
   // If they're not an admin don't let them see this component.
   const { user } = useSelector(state => state.user);
+  const books = useSelector(state => state.book.books);
+  const dispatch = useDispatch();
+  const [loading, setLoading ]= useState(false)
+
   if (!user.isAdmin) return <h2>You are not an admin, permission denied</h2>;
+
+  const bookHandler = async () =>{
+    try {
+      const {data} = await axios.get("/api/books");
+      dispatch(setBooks(data));
+      console.log(data);
+      setLoading(true);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    bookHandler();
+  }, [])
+
+  if(!loading){ return (<div><h1>Oops! Something went wrong!</h1></div>)}
 
   return (
     <div>
       <AdminNavbar props={user} />
-      <div>Maybe here will be a component to create a new book.</div>
-      Below will be a list of all books... maybe they will be links to more info
-      on them, and from there you can edit or delete.
-      <ul>
-        <li>devilman vol 1</li>
-        <li>wonder woman vol 89</li>
-      </ul>
+      <div className="adminProductPage">
+      <div><h1>Product List</h1></div>
+    <div className="adminProductContainer">
+      <table className="adminProductTable">
+        <tbody>
+          {books.map((book, index)=>{
+            return (
+              <tr className="adminProducttr" key={book.id}>
+                <td className="adminProducttd">
+                  {index}: {book.title} {book.volume}
+                </td>
+                <td className="adminProductButton">
+                  <button>Edit</button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+      </div>
     </div>
   );
 }
