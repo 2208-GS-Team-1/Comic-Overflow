a7c05b0566cd19d770b2e459c14f9393c70f74f8
Joshowens16 <joshowens16@gmail.com>
2022-12-13 17:12:20 -0800

    pagination is ugly but workinggit add .

--- src/components/Books/AllBooks.jsx
+++ src/components/Books/AllBooks.jsx
@@ -1,6 +1,6 @@
 
-import { Autocomplete, Card,  FormControl,  InputLabel,  MenuItem,  Select,  TextField,  Typography } from "@mui/material";
-
+import { Card,  FormControl,  InputLabel,  MenuItem,  Select, Typography } from "@mui/material";
+import ReactPaginate from 'react-paginate';
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useState } from "react";
@@ -20,6 +20,7 @@ const AllBooks = () => {
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
   const books = useSelector((state) => state.book.books);
+  const [currentPage, setCurrentPage] = useState(0);
   const [selectedSort,setSelectedSort] = useState('unsorted')
   const fetchBooks = async () => {
     setLoading(true);
@@ -31,6 +32,7 @@ const AllBooks = () => {
     }
     setLoading(false);
   };
+
   useEffect(() => {
     fetchBooks();
   }, []);
@@ -43,6 +45,9 @@ const AllBooks = () => {
   const handleSortChange = (event) => {
     setSelectedSort(event.target.value)
   }
+  function handlePageClick({ selected: selectedPage }) {
+    setCurrentPage(selectedPage);
+  }
   // 'sortedBooks' is the sorted array that we loop over to allow the user to sort by what they want
   let sortedBooks = [...books]
   // switch case that sorts the array based on the selected sort
@@ -50,31 +55,38 @@ const AllBooks = () => {
     switch(selectedSort){
       case 'ascending':
         sortedBooks.sort((a,b)=>(a.price > b.price) ? 1 : -1)
-      break
-      case "descending":
-        sortedBooks.sort((a,b)=>(a.price < b.price) ? 1 : -1)
-      break
-      case "unsorted":
-        sortedBooks = [...books]
-      break
-      case "a-z":
-        sortedBooks.sort(function(a, b){
-          if(a.title < b.title) { return -1; }
-          if(a.title > b.title) { return 1; }
-          return 0;
-      })
         break
-        case "z-a":
-          sortedBooks.sort(function(a, b){
-            if(a.title > b.title) { return -1; }
-            if(a.title < b.title) { return 1; }
-            return 0;
-        })
+        case "descending":
+          sortedBooks.sort((a,b)=>(a.price < b.price) ? 1 : -1)
           break
-    }
-  }
-  sortBooks()
-  return (
+          case "unsorted":
+            sortedBooks = [...books]
+            break
+            case "a-z":
+              sortedBooks.sort(function(a, b){
+                if(a.title < b.title) { return -1; }
+                if(a.title > b.title) { return 1; }
+                return 0;
+              })
+              break
+              case "z-a":
+                sortedBooks.sort(function(a, b){
+                  if(a.title > b.title) { return -1; }
+                  if(a.title < b.title) { return 1; }
+                  return 0;
+                })
+                break
+              }
+            }
+            sortBooks()
+            // PAGINATION
+            const PER_PAGE = 12;
+            const offset = currentPage * PER_PAGE;
+            const currentPageData = sortedBooks
+                .slice(offset, offset + PER_PAGE)
+            const pageCount = Math.ceil(sortedBooks.length / PER_PAGE);
+            //
+            return (
     <div className="productsContainer">
       <h1>All Comics</h1>
       <div className="sortBar">
@@ -94,13 +106,13 @@ const AllBooks = () => {
     </FormControl>
       </div>
     <div className="allBooks">
-      {sortedBooks.map((book) => {
+      {currentPageData.map((book) => {
         return (
           <Card
-            sx={{ boxShadow: 2 }}
-            className="productCard"
-            variant="outlined"
-            key={book.id}>
+          sx={{ boxShadow: 2 }}
+          className="productCard"
+          variant="outlined"
+          key={book.id}>
             <div className="productCardImg">
               <Link to={`/books/${book.id}`}>
                 <img src={book.imageURL} />
@@ -118,8 +130,18 @@ const AllBooks = () => {
           </Card>
         );
       })}
+    <ReactPaginate
+      previousLabel={"← Previous"}
+      nextLabel={"Next →"}
+      pageCount={pageCount}
+      onPageChange={handlePageClick}
+      containerClassName={"pagination"}
+      previousLinkClassName={"pagination__link"}
+      nextLinkClassName={"pagination__link"}
+      disabledClassName={"pagination__link--disabled"}
+      activeClassName={"pagination__link--active"}
+    />
     </div>
-
     </div>
   );
 };
