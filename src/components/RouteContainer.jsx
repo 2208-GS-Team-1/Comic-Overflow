6c6065a85d6faa456cc25a32eccc56d2d83b985c
Yijin Ro <yri.carmine@gmail.com>
2022-12-14 21:29:31 -0500

    initial set up

--- src/components/RouteContainer.jsx
+++ src/components/RouteContainer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
@@ -16,6 +17,7 @@ import AdminBooksPage from "./admin/AdminBooksPage";
 import AdminUsersPage from "./admin/AdminUsersPage";
 import AdminReviewsPage from "./admin/AdminReviewsPage";
 import AdminBookEdit from "./admin/AdminBookEdit.jsx";
+import AdminUserview from "./admin/AdminUserview.jsx";
 
 function RouteContainer({ user }) {
   return (
@@ -35,6 +37,7 @@ function RouteContainer({ user }) {
           <>
             <Route path="/admin" element={<AdminHomepage />} />
             <Route path="/admin/users" element={<AdminUsersPage />} />
+            <Route path="/admin/users/:id" element ={<AdminUserview />} />
             <Route path="/admin/books" element={<AdminBooksPage />} />
             <Route path="/admin/books/:id" element={<AdminBookEdit />} />
             <Route path="/admin/reviews" element={<AdminReviewsPage />} />
---
+++ src/components/admin/AdminUserview.jsx
@@ -0,0 +1,27 @@
+// import axios from 'axios';
+import React from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useNavigate, useParams } from 'react-router-dom';
+
+const AdminUserview = () => {
+    // const {id} = useParams();
+    // const dispatch = useDispatch();
+    // const navigate = useNavigate();
+
+    // const userHandler = async () =>{
+    //     try {
+    //         const {data} = await axios.get(`/api/users/${id}`)
+    //         console.log(data);
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
+
+    return (
+        <div>
+            <h1>TEST</h1>
+        </div>
+    );
+};
+
+export default AdminUserview;
