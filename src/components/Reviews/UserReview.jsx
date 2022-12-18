300b8c096054ef7917bb1908e4d8c9f8d37765f4
Yijin Ro <yri.carmine@gmail.com>
2022-12-18 17:59:13 -0500

    user review form set up with axios and states

--- src/components/Reviews/UserReview.jsx
+++ src/components/Reviews/UserReview.jsx
@@ -2,11 +2,9 @@
 import React, { useState } from 'react';
 import { Card } from "@mui/material";
 import { Rating } from "@mui/material";
-import { combineReducers } from 'redux';
-import { CompressOutlined } from '@mui/icons-material';
 import axios from 'axios';
 
-const UserReview = ({selectedBook, user}) => {
+const UserReview = ({selectedBook, user, singleBookHandler}) => {
 
     const [subject, setSubject] = useState("")
     const [content, setContent] = useState("");
@@ -24,34 +22,33 @@ const UserReview = ({selectedBook, user}) => {
 
     const ratingHandler = (event) => {
         setRating(event.target.value)
-        console.log(event.target.value)
     }
 
     const reviewMaker = async (event) => {
         event.preventDefault();
         const reviewData = { subject, content, rating, userId, bookId }
         await axios.post("/api/reviews", reviewData);
+        singleBookHandler();
     }
 
     return (
-        <div>
-        <Card className="cardForAllProducts" sx={{ boxShadow: 2 }}>
+        <div className='reviewformContainer'>
             <h1>Leave a Review</h1>
-            <div className='formBase'>
-                <form>
-                    <label>Subject</label>
-                    <input onChange={subjectHandler} />
-                    <label>Review for {selectedBook.title}</label>
+            <div className='reviewformBase'>
+                <form className='innerreviewformBase' onSubmit={reviewMaker}>
+                    <label className='reviewLabel'>Subject</label>
+                    <input onChange={subjectHandler}
+                    placeholder='Enter subject' />
+                    <label className='reviewLabel'>Review for {selectedBook.title}</label>
                     <textarea
                     onChange={contentHandler}
                     rows="10"
                     placeholder='Tell us what you think!'></textarea>
-                    <label>Rating</label>
+                    <label className='reviewLabel'>Rating</label>
                     <Rating name="no-value" defaultValue={1} onChange={ratingHandler} />
-                    <button type='submit'>Submit</button>
+                    <button className='reviewbutton' type='submit'>Submit</button>
                 </form>
             </div>
-            </Card>
         </div>
     );
 };
