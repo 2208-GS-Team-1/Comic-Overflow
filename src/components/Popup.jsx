a0f51f5c480ee02060f3c5dac4fd6ef2d6262a4a
Yijin Ro <yri.carmine@gmail.com>
2022-12-18 17:01:22 -0500

    messages been edited

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -4,9 +4,12 @@ import React from 'react';
 const Popup = (props) => {
     return(props.trigger)? (
         <div className='popup'>
-            <div className='popup_inner'>
+            <div className='popup_inner' >
                 <button className='popup_button' onClick={()=>{props.setPopTrigger(false)}}>Close</button>
-            <h1>Grand Opening Special!</h1>
+            <h1 className='popupH1'>Holiday Special!</h1>
+            <h2 className='popupH2'>We will donate 10% of the sale between </h2>
+            <h2 className='popupH2'>12/19 - 12/24 to families in need</h2>
+            <h2 className='popupH2'>Give the gift of Love!</h2>
             </div>
         </div>
     ): "";
