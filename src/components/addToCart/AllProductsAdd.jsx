c2827cbfb650a99322e84b73efad37242bff9e78
Rosalie Newman <rosalienewman0@gmail.com>
2022-12-19 15:55:30 -0500

    fixed merge conflicts with AllProductsAdd.jsx

--- src/components/addToCart/AllProductsAdd.jsx
+++ src/components/addToCart/AllProductsAdd.jsx
@@ -5,19 +5,16 @@ import { AddShoppingCart } from "@mui/icons-material";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { setCart, addBookToCart } from "../../store/cartSlice";
-<<<<<<< HEAD
 import ProductionQuantityLimitsIcon from "@mui/icons-material/ProductionQuantityLimits";
-=======
-import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
 import { useState } from "react";
->>>>>>> origin/main
+
 const AllProductsAdd = ({ book }) => {
-  const [open, setOpen] = useState(false)
-  const [alertType, setAlertType] = useState('success')
-  const [alertMessage, setAlertMessage] = useState('')
-  const { user } = useSelector(state => state.user);
+  const [open, setOpen] = useState(false);
+  const [alertType, setAlertType] = useState("success");
+  const [alertMessage, setAlertMessage] = useState("");
+  const { user } = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  const { cart } = useSelector(state => state.cart);
+  const { cart } = useSelector((state) => state.cart);
   const userId = user.id;
   const loadCartFromLocalStorage = () => {
     // Get the stringified cart from local storage
@@ -35,7 +32,7 @@ const AllProductsAdd = ({ book }) => {
       dispatch(setCart(cart));
     }
   };
-  const saveCartToLocalStorage = cart => {
+  const saveCartToLocalStorage = (cart) => {
     // Local storage can only store strings, so we need to convert the cart object to a string
     const cartString = JSON.stringify(cart);
 
@@ -43,114 +40,62 @@ const AllProductsAdd = ({ book }) => {
     localStorage.setItem("cart", cartString);
   };
   const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
     setOpen(false);
-  }
+  };
 
   useEffect(() => {
     loadCartFromLocalStorage();
   }, []);
   const addToCart = async () => {
-<<<<<<< HEAD
     try {
-      // If it is a guest user adding to cart... (not logged in)
       if (!user.id) {
         const existingItem = cart.find(
-          cartItem => cartItem.book.id === book.id
+          (cartItem) => cartItem.book.id === book.id
         );
         if (
           existingItem &&
           existingItem.book.stock >= existingItem.quantity + 1
         ) {
           const updatedQuantity = existingItem.quantity + 1;
-          const newCart = cart.map(item => {
+          const newCart = cart.map((item) => {
             if (item.book.id === existingItem.book.id) {
               return {
                 ...item,
                 quantity: updatedQuantity,
               };
-=======
-      try {
-          if (!user.id) {
-              const existingItem = cart.find((cartItem) => cartItem.book.id === book.id);
-              if(existingItem && existingItem.book.stock >= existingItem.quantity + 1) {
-                const updatedQuantity = existingItem.quantity + 1
-                const newCart = cart.map(item => {
-                    if (item.book.id === existingItem.book.id) {
-                        return {
-                            ...item,
-                            quantity: updatedQuantity,
-                        };
-                    }
-                    return item;
-                });
-                dispatch(setCart(newCart))
-                saveCartToLocalStorage(newCart)
-                setAlertType('success')
-                setAlertMessage('Added to cart!')
-                setOpen(true)
-            }else 
-            if(!existingItem) {
-                const { data }= await axios.get(`/api/books/${book.id}`)
-                const bookAndQuantity = {quantity: 1, book:data}
-                dispatch(addBookToCart(bookAndQuantity))
-                saveCartToLocalStorage([...cart, bookAndQuantity])
-                setAlertType('success')
-                setAlertMessage('Added to cart!')
-                setOpen(true)
-            } else {
-              setAlertType('warning')
-              setAlertMessage('Not enough stock!')
-              setOpen(true)
-            }
-        } else {
-            const existingItem = cart.find((cartItem) => cartItem.book.id === book.id);
-            if(existingItem && existingItem.book.stock >= existingItem.quantity + 1) {
-                await axios.put(`/api/cart/${existingItem.id}`, {
-                    quantity: existingItem.quantity + 1,
-                });
-                const updatedCart = await axios.get(`/api/cart/user/${user.id}`)
-                dispatch(setCart(updatedCart.data))
-                saveCartToLocalStorage(updatedCart.data)
-                setAlertType('success')
-                setAlertMessage('Added to cart!')
-                setOpen(true)
-            } else if (!existingItem){
-                const bookId = book.id
-                const body = { userId, bookId };
-                await axios.post("/api/cart", body);
-                const updatedCart = await axios.get(`/api/cart/user/${user.id}`)
-                dispatch(setCart(updatedCart.data))
-                saveCartToLocalStorage(updatedCart.data)
-                setAlertType('success')
-                setAlertMessage('Added to cart!')
-                setOpen(true)
-            } else {
-              setAlertType('warning')
-              setAlertMessage('Not enough stock!')
-              setOpen(true)
->>>>>>> origin/main
             }
             return item;
           });
           dispatch(setCart(newCart));
           saveCartToLocalStorage(newCart);
+          setAlertType("success");
+          setAlertMessage("Added to cart!");
+          setOpen(true);
         } else if (!existingItem) {
           const { data } = await axios.get(`/api/books/${book.id}`);
           const bookAndQuantity = { quantity: 1, book: data };
           dispatch(addBookToCart(bookAndQuantity));
           saveCartToLocalStorage([...cart, bookAndQuantity]);
+          setAlertType("success");
+          setAlertMessage("Added to cart!");
+          setOpen(true);
+        } else {
+          setAlertType("warning");
+          setAlertMessage("Not enough stock!");
+          setOpen(true);
         }
       } else {
-        // If the user IS logged, we need to create/update cartItem in the backend
+        // If user logged in...
+
         // JWT & authorization header to give for authorization check in the API
         const token = window.localStorage.getItem("token");
         const config = { headers: { authorization: "Bearer " + token } };
 
         const existingItem = cart.find(
-          cartItem => cartItem.book.id === book.id
+          (cartItem) => cartItem.book.id === book.id
         );
         if (
           existingItem &&
@@ -158,7 +103,9 @@ const AllProductsAdd = ({ book }) => {
         ) {
           await axios.put(
             `/api/cart/${existingItem.id}`,
-            { quantity: existingItem.quantity + 1 },
+            {
+              quantity: existingItem.quantity + 1,
+            },
             config
           );
           const updatedCart = await axios.get(
@@ -167,6 +114,9 @@ const AllProductsAdd = ({ book }) => {
           );
           dispatch(setCart(updatedCart.data));
           saveCartToLocalStorage(updatedCart.data);
+          setAlertType("success");
+          setAlertMessage("Added to cart!");
+          setOpen(true);
         } else if (!existingItem) {
           const bookId = book.id;
           const body = { userId, bookId };
@@ -177,6 +127,13 @@ const AllProductsAdd = ({ book }) => {
           );
           dispatch(setCart(updatedCart.data));
           saveCartToLocalStorage(updatedCart.data);
+          setAlertType("success");
+          setAlertMessage("Added to cart!");
+          setOpen(true);
+        } else {
+          setAlertType("warning");
+          setAlertMessage("Not enough stock!");
+          setOpen(true);
         }
       }
     } catch (error) {
@@ -185,7 +142,6 @@ const AllProductsAdd = ({ book }) => {
   };
   return (
     <>
-<<<<<<< HEAD
       <Button
         disabled={book.stock === 0 && true}
         size="small"
@@ -196,26 +152,22 @@ const AllProductsAdd = ({ book }) => {
         ) : (
           <ProductionQuantityLimitsIcon />
         )}
-=======
-      <Button 
-      disabled={book.stock === 0 && true}
-      size="small" onClick={addToCart}>
-        {book.stock !== 0 ? <AddShoppingCart /> : <ProductionQuantityLimitsIcon/> }
->>>>>>> origin/main
       </Button>
-      <Snackbar
-              open={open}
-              autoHideDuration={3000}
-              onClose={handleClose}
-              >
-              <Alert sx={{ display: "flex", justifyContent: "center",minWidth:"200px", fontFamily: "'Dogfish', sans-serif"}} 
-              variant="filled" 
-              severity={alertType}>
-                {alertMessage}
-              </Alert>
-          </Snackbar>
+      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+        <Alert
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            minWidth: "200px",
+            fontFamily: "'Dogfish', sans-serif",
+          }}
+          variant="filled"
+          severity={alertType}
+        >
+          {alertMessage}
+        </Alert>
+      </Snackbar>
     </>
-
   );
 };
 
