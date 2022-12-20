2e81fbc80ba9f8a223cb985b7e2c6ecf5ba021cc
Yijin Ro <yri.carmine@gmail.com>
2022-12-19 20:48:36 -0500

    issue fixed. token added

--- src/components/admin/AdminUserEdit.jsx
+++ src/components/admin/AdminUserEdit.jsx
@@ -1,17 +1,19 @@
 /* eslint-disable react/prop-types */
 import axios from 'axios';
 import React, { useState } from 'react';
+import { Alert } from "@mui/material";
 
 
 const AdminUserEdit = ({ fetchedUser, userHandler }) => {
 
     const [firstName , setFirstname] = useState(`${fetchedUser.firstName}`)
     const [lastName, setLastName] = useState(`${fetchedUser.lastName}`)
-    const [address, setAddress] = useState(`${fetchedUser.address}`)
+    const [address, setAddress] = useState("")
     const [birthday, setBirthday] = useState(`${fetchedUser.birthday}`)
-    const [email, setEmail] = useState(`${fetchedUser.email}`)
+    const [email, setEmail] = useState("")
     const [phoneNumber, setPhoneNumber] = useState(`${fetchedUser.phoneNumber}`)
    const [isDeactivated, setDeactivated] = useState(fetchedUser.isDeactivated);
+   const [errorMessage, setErrorMessage] = useState(false);
 
 
     const firstNameHandler = (event) => {
@@ -36,10 +38,13 @@ const AdminUserEdit = ({ fetchedUser, userHandler }) => {
         setDeactivated(event.target.value)
     }
 
+
     const updateHandler = async (event) =>{
         event.preventDefault();
+        const token = window.localStorage.getItem("token");
+        const config = { headers: { authorization: "Bearer " + token } };
         const updatedData = {address, email, phoneNumber, firstName, lastName, birthday, isDeactivated}
-        await axios.put(`/api/users/${fetchedUser.id}`, updatedData)
+        await axios.put(`/api/users/${fetchedUser.id}`, updatedData, config)
         userHandler();
     }
 
@@ -49,37 +54,44 @@ const AdminUserEdit = ({ fetchedUser, userHandler }) => {
                 <form onSubmit={updateHandler}>
                     <h1>EDIT INFO</h1>
                     <div className='formInput'>
+                        {/*first name input */}
                         <p className='adminEditP'>First Name:  
                         {firstName.length < 1 ? " This is a required field" : ""}</p>
                         <input className='adminEditInput'
-                        value={firstName}
+                        placeholder={firstName}
                         onChange={firstNameHandler} />
+                        {/*last name input */}
                         <p className='adminEditP'>Last Name: 
                         {lastName.length < 1 ? "this is a required field" : ""}</p>
                         <input className='adminEditInput'
-                        value={lastName}
+                         placeholder={lastName}
                         onChange={lastNameHandler} />
-                        <p className='adminEditP'>Address:</p>
+                        {/*address input */}
+                        <p className='adminEditP'>Address: <small>{ address.length < 1 ? " Address cannot be empty" : ""}</small></p>
                         <input className='adminEditInput'
                         type="text"
-                        value={fetchedUser.address ? address : ""}
+                         placeholder="Please re-enter your address"
                         onChange={addressHandler} />
+                        {/*birthday input */}
                         <p className='adminEditP'>Birthday:</p>
                         <p className='adminEditP'>
                             <small>Please follow the format xxxx-xx-xx</small>
                         </p>
                         <input className='adminEditInput'
-                        value={(birthday.length <= 4)? '0000-00-00': birthday}
+                        placeholder ="0000-00-00"
                         onChange={birthdayHandler} />
+                        {/*email input */}
                         <p className='adminEditP'>Email:</p>
                         <input className='adminEditInput'
-                        value={email} 
+                        placeholder="Please re-enter your email"
                         onChange={emailHandler}/>
+                        {/*phone number input */}
                         <p className='adminEditP'>Phone Number: <small>{(phoneNumber.length > 10 || phoneNumber.length < 10)? "Please enter 10 digit numbers": ""}</small></p>
                         <input className='adminEditInput'
                         type="number"
-                        value={phoneNumber}
+                        placeholder={phoneNumber}
                         onChange={phoneNumberHandler} />
+                        {/*deactivation status input */}
                         <p className='adminEditP'>Deactivation Status</p>
                         <p className='adminEditSmallP'><small>Enter True or False</small></p>
                         <input className='adminEditInput'
