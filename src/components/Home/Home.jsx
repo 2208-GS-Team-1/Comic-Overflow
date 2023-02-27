9cc39445d89f4f46aa8aeac806a53b1477ebd595
Joshowens16 <joshowens16@gmail.com>
2023-02-27 09:59:16 -0800

    fixing margin

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,17 +5,18 @@ import "./homeStyle.css";
 import StoreOpenBanner from "../storeOpenBanner/StoreOpenBanner.jsx";
 
 const Home = () => {
-
-  const [ popTrigger, setPopTrigger] = useState(false);
-  useEffect(()=>{
-    setTimeout( ()=>{setPopTrigger(true)}, 1500);
-  }, [])
+  const [popTrigger, setPopTrigger] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setPopTrigger(true);
+    }, 1500);
+  }, []);
 
   return (
     <div>
       <div>
         <Slider />
-        <Popup trigger= {popTrigger} setPopTrigger = {setPopTrigger} />
+        {/* <Popup trigger= {popTrigger} setPopTrigger = {setPopTrigger} /> */}
         <StoreOpenBanner />
       </div>
     </div>
